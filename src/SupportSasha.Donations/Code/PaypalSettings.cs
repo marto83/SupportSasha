fc50a25b7fefdfe9ef0d8c995f9240346a4921bd
Martin Georgiev <marto83@gmail.com>
2012-07-29 22:31:47 +0100

    Added paypal settings

---
+++ src/SupportSasha.Donations/Code/PaypalSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace SupportSasha.Donations.Code
+{
+    public static class PaypalSettings
+    {
+        public static bool IsSandbox
+        {
+            get { return Convert.ToBoolean(ConfigurationManager.AppSettings["Paypayl:Sandbox"]); }
+        }
+
+        public static string PaypalUrl
+        {
+            get { return IsSandbox ? "https://www.sandbox.paypal.com" : "https://www.paypal.com"; }
+        }
+
+        public static string BusinessEmail
+        {
+            get { return ConfigurationManager.AppSettings["Paypal:BusinessEmail"]; }
+        }
+
+        public static string AuthToken
+        {
+            get { return ConfigurationManager.AppSettings["Paypal:AuthToken"]; }
+        }
+
+    }
+}
