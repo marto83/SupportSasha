4a4d0965c69dfb7b01c9f2baed215e6b8b5c9fdb
Martin Georgiev <marto83@gmail.com>
2013-04-20 21:37:55 +0100

    Added Ilian's campaign

--- src/SupportSasha.Donations/Controllers/FundraisingController.cs
+++ src/SupportSasha.Donations/Controllers/FundraisingController.cs
@@ -37,5 +37,15 @@ namespace SupportSasha.Donations.Controllers
             model.CurrentAmount = (int)Math.Round(donations.Sum(x => x.Amount), 0);
             return View(model);
         }
+
+        public ActionResult Ilian()
+        {
+            var donations = RavenSession.Query<Donation>().Where(x => x.CampaignName == "Ilian" && x.Confirmed).OrderByDescending(x => x.Date).ToList();
+            var model = new EventProgressModel();
+            model.Donations = donations.OrderByDescending(x => x.Date);
+            model.Target = 1000;
+            model.CurrentAmount = (int)Math.Round(donations.Sum(x => x.Amount), 0);
+            return View(model);
+        }
     }
 }
